caade7814e7df1e7b87982bab709e3b5265123bf
daichi takei <dadewujing0@gmail.com>
2025-04-01 00:12:19 +0900

    update: データの型の変更

--- app/controllers/onedays_controller.rb
+++ app/controllers/onedays_controller.rb
@@ -7,7 +7,7 @@ class OnedaysController < ApplicationController
     @posts = Post.where("DATE(start_time) = ?", "#{params[:date]}%")
     @today_memos = Memo.where("DATE(created_at) = ?", "#{params[:date]}%")
     @one_diaries = OneDiary.where("DATE(created_at) = ?", "#{params[:date]}%")
-  end 
+  end
 
   private
     def oneday_params
