da668a9ec124175541016071c8300b12dc4390af
daichi takei <dadewujing0@gmail.com>
2025-04-01 00:07:35 +0900

    update: date型を変更

--- app/controllers/onedays_controller.rb
+++ app/controllers/onedays_controller.rb
@@ -4,7 +4,7 @@ class OnedaysController < ApplicationController
   end
 
   def concrete
-    @posts = Post.where('start_time LIKE ?', "#{params[:date]}%")
+    @posts = Post.where("DATE(start_time) = ?", "#{params[:date]}%")
     @today_memos = Memo.where("DATE(created_at) = ?", "#{params[:date]}%")
     @one_diaries = OneDiary.where("DATE(created_at) = ?", "#{params[:date]}%")
   end 
